55a2c41b273c60038054abdbe6d9317ac8b0e1d7
Delvin <179110642+dvnn-star@users.noreply.github.com>
2026-05-01 20:56:58 +0700

    resolve

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,8 +20,8 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('components/navbar');
 		$this->load->view('templates/header.php');
+		$this->load->view('components/navbar');
 		$this->load->view('components/hero');
 		$this->load->view('welcome_message');
 	}
