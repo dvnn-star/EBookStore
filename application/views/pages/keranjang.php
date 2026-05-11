2727bec7b741c1f51d911aafcb4cf1eff5de2ba0
AdiDharma14 <adidarma1404@gmail.com>
2026-05-12 00:38:01 +0700

    commit car & keranjang

---
+++ application/views/pages/keranjang.php
@@ -0,0 +1,6 @@
+<?php
+    $this->load->view('templates/header');
+    $this->load->view('components/navbar');
+    $this->load->view('components/cart');
+    $this->load->view('components/footer');
+?>
